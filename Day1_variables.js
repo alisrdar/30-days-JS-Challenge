8362f51c65f4bcb236f879dde6545dd78a14a615
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-14 02:28:59 +0500

    Create Day1_variables.js
    All the tasks...
    Plus If someone is reading this, What were the 
    
    Features:
    Create script to declare variables was about

---
+++ Day1_variables.js
@@ -0,0 +1,33 @@
+// Act 1
+var num=4;
+console.log(num);
+let str="Day 1 of 30, lets go!";
+console.log(str);
+
+// Act 2
+const bool= true;
+console.log(bool);
+
+// Act 3
+let student ={
+    Name: "Sifu",
+    title: "Master",
+    rank: 3
+}
+
+let bevrages=["Chai", "Coke", "Coffee", "Juice"];
+console.log(typeof num, typeof str, typeof bool, typeof student, typeof bevrages );
+
+// Act 4
+let a=8;
+console.log(a);
+a=8;
+console.log(a);
+
+//Act 5
+const b=7;
+console.log(b);
+b=5;
+console.log(b);
+// error TypeError: Assignment to constant variable.
+// const dataType can't be reassigned!
