316bb73fe8b46ace3f6598d0e60d225e535921ad
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-18 20:35:35 +0500

    Create feature1.js

---
+++ Day2/feature1.js
@@ -0,0 +1,31 @@
+//Task 1
+const num1 = 10;
+const num2 = 7;
+const sum = num1 + num2;
+console.log("The sum of the two numbers is equal to:",sum);
+
+//Task 2
+const diff= num1 - num2;
+console.log("The difference of the two numbers is equal to:",diff);
+
+//Task 3
+const mul= num1 * num2;
+console.log("The product of the two numbers is equal to:",mul);
+
+//Task 4
+const divide= num1 / num2;
+console.log("The result of division of the two numbers is equal to:",divide);
+
+//Task 5
+const remainder = num1 % num2;
+console.log(remainder);
+
+//Task 6
+//num1 = 10
+console.log("variable num:", num1);
+num1 +=7;
+console.log("new value after +=:", num1);
+
+//Task 7
+num1 -=5;
+console.log("new value after -=", num1);
