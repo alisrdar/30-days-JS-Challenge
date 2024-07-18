3a333e1c4fdd2d3a3329e25db889c9cb971b6791
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-18 19:44:07 +0500

    Create allTasks.js
    All the tasks of Day2 Operators

---
+++ Day2/allTasks.js
@@ -0,0 +1,71 @@
+//Acctivity 1
+//Task 1
+const num1 = 10;
+const num2 = 7;
+const sum = num1 + num2;
+console.log("The sum of the two numbers is equal to:",sum);
+
+//Task 2
+const diff= num1 - num2;
+console.log("The difference of the two numbers is equal to:",diff);
+
+//Task 3
+const mul= num1 * num2;
+console.log("The product of the two numbers is equal to:",mul);
+
+//Task 4
+const divide= num1 / num2;
+console.log("The result of division of the two numbers is equal to:",divide);
+
+//Task 5
+const remainder = num1 % num2;
+console.log(remainder);
+
+//Activity 2
+//Task 6
+let a=8;
+console.log("variable a:",a);
+a+=7;
+console.log("new value after +=:",a);
+
+//Task 7
+a-=5;
+console.log("new value after -=",a);
+
+//Activity 3
+//Task 8
+let comp= 7 > 5;
+console.log("Less than:",comp);
+
+console.log("Greater than:", 7 < 5);
+
+//Task 9
+console.log("Greater than equal to:", 9 >= 9 );
+console.log("Less than equal to:", 8 <= 9 );
+
+//Task 10
+console.log('7 == "7":' ,7 == "7");
+console.log('7 === "7":' ,7 === "7");
+
+//Activity 4
+//Task 11
+let compareAnd = true && false;
+console.log(compareAnd);
+
+//Task 12
+let compareOr = 1 || 0;
+console.log(compareOr);
+
+//Task 13
+let compareNot = !compareAnd;
+console.log(compareNot);
+
+//Task 14
+let numA= -9
+let numB= 4;
+
+let ter = numA > 0 ? " is positive number": "is negative number";
+console.log(numA, ter);
+ 
+ter = (numB > 0) ? " is positive number": "is negative number";
+console.log(numB, ter);
