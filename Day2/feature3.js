0680c2f17b420342962dc428675136c8c0d49337
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-18 20:22:06 +0500

    Create feature3.js

---
+++ Day2/feature3.js
@@ -0,0 +1,8 @@
+let numA= -9
+let numB= 4;
+
+let ter = (numA > 0) ? " is positive number": "is negative number";
+console.log(numA, ter);
+ 
+ter = (numB > 0) ? " is positive number": "is negative number";
+console.log(numB, ter);
