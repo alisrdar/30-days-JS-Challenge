6773f60808016b50200d2d7af960d8345ef38741
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-18 20:17:28 +0500

    Create feature2.js

---
+++ Day2/feature2.js
@@ -0,0 +1,24 @@
+const num1 = 8;
+const num2 = 5;
+
+//Comparison Operators
+let comp = num1 < num2;
+console.log("Less than:",comp);
+
+comp = num1 > num2;
+console.log("Greater than:", comp);
+
+//Task 9
+comp = num1 >= num2;
+console.log("Greater than equal to:", comp);
+
+comp = num1 <= num2;
+console.log("Less than equal to:", comp);
+
+//Task 10
+let num = 9;
+comp = num == "9";
+console.log('num == "9":' ,comp);
+
+comp = num === "9";
+console.log('num === "9":' ,comp);
