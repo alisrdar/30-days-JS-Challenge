4e08fa22420093cf4cb57496d6f04a4af1d20b9c
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-18 19:53:35 +0500

    Update allTasks.js

--- Day2/allTasks.js
+++ Day2/allTasks.js
@@ -1,4 +1,4 @@
-//Acctivity 1
+//Activity 1
 //Task 1
 const num1 = 10;
 const num2 = 7;
@@ -6,21 +6,20 @@ const sum = num1 + num2;
 console.log("The sum of the two numbers is equal to:",sum);
 
 //Task 2
-const diff= num1 - num2;
+const diff= 27 - 9;
 console.log("The difference of the two numbers is equal to:",diff);
 
 //Task 3
-const mul= num1 * num2;
+const mul= 9 * 7;
 console.log("The product of the two numbers is equal to:",mul);
 
 //Task 4
-const divide= num1 / num2;
+const divide= 54 / 5;
 console.log("The result of division of the two numbers is equal to:",divide);
 
 //Task 5
 const remainder = num1 % num2;
 console.log(remainder);
-
 //Activity 2
 //Task 6
 let a=8;
