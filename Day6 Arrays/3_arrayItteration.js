4596a48b655e64d678307ba027114cc9c57a0241
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-20 02:08:51 +0500

    Update 3_arrayItteration.js

--- Day6 Arrays/3_arrayItteration.js
+++ Day6 Arrays/3_arrayItteration.js
@@ -1,1 +1,12 @@
+// The array
+const array2 =["Chai", "Aur", "Code", "Day6"]
 
+// For
+for (let i = 0; i < array2.length; i++) {
+   console.log(array2[i])
+}
+
+// For each
+array2.forEach((val) => {
+    console.log(val);
+});
