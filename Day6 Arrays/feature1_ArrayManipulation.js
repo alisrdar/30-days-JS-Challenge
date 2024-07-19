63974e6b8981162fdbeebbf2dd7287aeeb842dd1
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-20 02:01:48 +0500

    Create feature1_ArrayManipulation.js
    first feature

---
+++ Day6 Arrays/feature1_ArrayManipulation.js
@@ -0,0 +1,20 @@
+// Array creation
+let arr = [1,2,3,4,5];
+console.log(arr);
+
+// Methods
+// Task 3
+arr.push(6);
+console.log("Push", arr);
+
+// Task 4
+arr.pop();
+console.log("Pop", arr);
+
+// Task 5
+arr.shift();
+console.log("Shift", arr);
+
+// Task 6
+arr.unshift(7)
+console.log("Unshift", arr);
