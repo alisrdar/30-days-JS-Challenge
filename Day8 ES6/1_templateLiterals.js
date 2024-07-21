a891b752fa68635db8fc0a42e6120171c2d6621a
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-21 14:52:26 +0500

    Create 1_templateLiterals.js
    Feature 1

---
+++ Day8 ES6/1_templateLiterals.js
@@ -0,0 +1,16 @@
+// --------------------------> Feature 1 <--------------------------------
+// template Literals
+const Name = "Ali";
+let age = 21;
+
+let str = `Hello! my name is ${Name} & I'm ${age} years old!`;
+console.log(str);
+
+// Multiline String
+let multiStr = `Important topics covered so far in 30 challenge 
+- loops 
+- Functions  \n -Arrays 
+- Objects 
+- ES6+ fetures`;
+
+console.log(multiStr);
