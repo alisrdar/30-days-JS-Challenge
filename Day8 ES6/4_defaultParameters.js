6e180c6db988205b84e5213040893e426016b71b
alisrdar <148740583+alisrdar@users.noreply.github.com>
2024-07-21 14:59:21 +0500

    Create 4_defaultParameters.js
    Feature 4

---
+++ Day8 ES6/4_defaultParameters.js
@@ -0,0 +1,8 @@
+// --------------------------> Feature 4 <--------------------------------
+// default param
+function product(num1, num2=1) {
+    return num1 * num2;
+}
+
+console.log(product(4));
+console.log(product(8,6));
